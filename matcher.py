7dcd8224584139f2deb2d5a7fccce70bdc2508af
yuluomeng <yuluomeng@gmail.com>
2017-03-08 18:16:48 -0500

    Read filename and search term from command line

--- matcher.py
+++ matcher.py
@@ -1,6 +1,7 @@
 
 from collections import defaultdict
 from itertools import combinations_with_replacement
+import sys
 
 import numpy as np
 from scipy import sparse
@@ -10,15 +11,20 @@ from scipy.sparse import linalg
 DIMS = 10
 
 
+def read_document(filename):
+
+    with open(filename) as f:
+        tweets = map(lambda s: s.strip().split(), f.readlines())
+        docs = [set(map(lambda s: s.lower(), tweet)) for tweet in tweets]
+
+    return docs
+
+
 def probabilities(keywords, docs):
-    """Calculate the PPMI between the keywords of the given documents.
+    """Find counts and associations between the keywords of the given documents.
 
-    each entry is the number of times we've the two words together.
-    how do we generate this efficiently?
-    matrix =
-
-    mapping of keyword to the number of times we've seen that keyword
-    counts = {keyword: += 1}
+    counts maps a word -> number of times it appears
+    associations maps (word1, word2) -> number of times they appear together
     """
     counts = defaultdict(int)
     associations = defaultdict(int)
@@ -38,8 +44,8 @@ def probabilities(keywords, docs):
 def build_matrix(lookup, keywords, counts, assocs):
     """Build the PPMI matrix."""
 
-    matrix = sparse.dok_matrix((len(keywords), len(keywords)), dtype=np.float64)
     keyword_count = len(keywords)
+    matrix = sparse.dok_matrix((keyword_count, keyword_count), dtype=np.float64)
 
     for word1, word2 in combinations_with_replacement(keywords, 2):
         word1_idx = lookup[word1]
@@ -48,17 +54,16 @@ def build_matrix(lookup, keywords, counts, assocs):
         num = assocs[(word1, word2)] * keyword_count
         denom = counts[word1] * counts[word2]
 
-        pmi = np.log(num / denom)
+        pmi = np.log(num / denom) if num > 0 else 0
         ppmi = max(0, pmi)
 
         matrix[word1_idx, word2_idx] = ppmi
         matrix[word2_idx, word1_idx] = ppmi
 
-    return matrix.tocsc()
+    return matrix
 
 
 def build_word_vec(lookup, word):
-
     vec = np.zeros(len(lookup))
     word_idx = lookup[word]
 
@@ -66,7 +71,7 @@ def build_word_vec(lookup, word):
     return vec
 
 
-def match(word_vec, u, s, v):
+def calc_similarities(word_vec, u, s, v):
     """Find the closest vectors."""
 
     vec_in_basis = np.dot(np.dot(s, u.transpose()), word_vec)
@@ -82,31 +87,31 @@ def match(word_vec, u, s, v):
 
 
 def display_likeness(lookup, similarities):
-    reverse_lookup = {idx: word for word, idx in lookup.items()}
+    idx_lookup = {idx: word for word, idx in lookup.items()}
     ordered = [
-        (reverse_lookup[i], likeness) for i, likeness in enumerate(similarities)]
+        (idx_lookup[i], likeness) for i, likeness in enumerate(similarities)]
 
     for word, likeness in sorted(ordered, key=lambda x: x[1], reverse=True):
         print(word, likeness)
 
 
 if __name__ == '__main__':
-    with open('documents.txt') as f:
-        tweets = map(lambda s: s.strip().split(), f.readlines())
-        docs = [set(map(lambda s: s.lower(), tweet)) for tweet in tweets]
 
+    filename, search_term = sys.argv[1], sys.argv[2]
+
+    docs = read_document(filename)
     keywords = set.union(*docs)
 
     counts, associations = probabilities(keywords, docs)
 
     lookup = {word: i for i, word in enumerate(keywords)}
+
     matrix = build_matrix(lookup, keywords, counts, associations)
+    u, s, v = linalg.svds(matrix, k=DIMS)
 
     # np.savetxt('test.csv', matrix.todense(), delimiter=',')
 
-    word_vec = build_word_vec(lookup, 'apple')
-    u, s, v = linalg.svds(matrix, k=DIMS)
-
-    similarities = match(word_vec, u, np.diag(s), v)
+    word_vec = build_word_vec(lookup, search_term)
+    similarities = calc_similarities(word_vec, u, np.diag(s), v)
 
     display_likeness(lookup, similarities)
